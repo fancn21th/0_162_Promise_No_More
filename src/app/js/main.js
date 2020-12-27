d9ea1140f6d44dfe7b0dd02015ffed5e1d1987de
范轶洁 <fanyjb@digitalchina.com>
2020-12-27 20:56:53 +0800

    a simple promise chain

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const API_URL = "https:/starwars.egghead.training/";
+
+const output = document.getElementById("app");
+
+const render = (content = "loading...") => (output.innerText = content);
+
+const getFirmTitles = (films) => {
+  return films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map((film) => `${film.episode_id}. ${film.title}`)
+    .join("\n");
+};
+
+render();
+
+fetch(`${API_URL}films`)
+  .then((response) => response.json())
+  .then((films) => {
+    render(getFirmTitles(films));
+  });
