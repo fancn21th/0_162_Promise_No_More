795adefe7465476ab24af16b2ed885ad84bcfdc9
范轶洁 <fanyjb@digitalchina.com>
2020-12-29 15:51:50 +0800

    promise constructor

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,31 +1,14 @@
-//#region Setup
-const API_URL = "https:/starwars.egghead.training/";
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 1000);
+});
 
-const output = document.getElementById("app");
-const loading = document.getElementById("loading");
+promise.then(
+  () => console.log("fulfilled"),
+  () => console.log("rejected")
+);
 
-const render = (content = "loading...") => (output.innerText = content);
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const getFirmTitles = (films) => {
-  return films
-    .sort((a, b) => a.episode_id - b.episode_id)
-    .map((film) => `${film.episode_id}. ${film.title}`)
-    .join("\n");
-};
-//#endregion
-
-render();
-
-Promise.resolve($.getJSON(`${API_URL}films`))
-  .then((films) => {
-    render(getFirmTitles(films));
-  })
-  .catch((error) => {
-    console.warn(error);
-    render(":(");
-    return [];
-  })
-  .finally(() => {
-    loading.remove();
-  })
-  .then((films) => console.log(films));
+sleep(1000).then(() => console.log("sleep 1s ... then fulfilled"));
